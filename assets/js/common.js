5a2607a7b53832ced3e26efcd8e9a303c27f2152
Ludric Paidius <K.a.norman@live.com>
2013-06-03 21:44:45 -0700

    Changed puff to slide

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     var current = $("#about");
     
-    function swap(curent, data) {
+    function swap(current, data) {
         next = $("<div>" + data + "</div>").css("opacity",0).addClass("box slide");
-        current.hide('puff', {}, 400, function() {
+        current.hide('slide', {}, 400, function() {
             $(this).parent().append(next);
             next.show().animate({opacity: 1});
             }
