69f8e295df17f88633d3ecffa7fa2585b1d1ec84
thefinn93 <thefinn93@thefinn93.com>
2013-06-06 14:17:01 -0700

    Switched the jquery to iterate through every navigation link, instead of manually specifying each one

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,18 +3,18 @@ $(document).ready(function(){
     var current = $("#placeholder");
     $(window).bind('popstate',  
         function(event) {
-            path = event.currentTarget.location.pathname.replace("/","");
+            path = event.currentTarget.location.pathname.split("/")[-1]
             if(path == "" || event.currentTarget == undefined) {
                 path = "index";
             }
-            $.get("/includes/" + path + '.html',function(data){
+            $.get("includes/" + path + '.html',function(data){
                 current = swap(current, data);
             });
         });
     function swap(curent, data, name) {
         var options = {direction: "left"}
         if(name != undefined) {
-            window.history.pushState(document.state, name, "/" + name);
+            window.history.pushState(document.state, name, name);
         } else {
             var options = {direction: "right"}
         }
@@ -26,29 +26,12 @@ $(document).ready(function(){
         );
         return next;
     }
-	$('#haverbeke').click(function(){
-		$.get('/includes/haverbeke.html',function(data){
-            current = swap(current, data, "haverbeke");
-		});
-	});
-	$('#cruse').click(function(){
-		$.get('/includes/cruse.html',function(data){
-            current = swap(current, data, "cruse");
-		});
-	});
-	$('#devlin').click(function(){
-		$.get('/includes/devlin.html',function(data){
-            current = swap(current, data, "devlin");
-		});
-	});
-	$('#Browsers').click(function(){
-		$.get('/includes/webbrowser.html',function(data){
-            current = swap(current, data, "webbrowser");
-		});
-	});
-	$('#fzilla').click(function(){
-		$.get('/includes/filezilla.html',function(data){
-            current = swap(current, data, "fzilla");
-		});
-	});
+    $(".menu a").each(function(key, item) {
+        $(item).click(function() {
+            $.get("includes/" + $(item).attr("href") + ".html", function(data) {
+                current = swap(current, data, $(item).attr("href"));
+            });
+            return false;
+        });
+    });
 });
